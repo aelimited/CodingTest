fa67318e005402b0e48ccf7543e1ae723f36761f
aelim <cloim09@gmail.com>
2023-11-23 10:56:20 +0900

    CommonMultiple_coding test

---
+++ JavaTest/CommonMultiple.java
@@ -0,0 +1,11 @@
+package JavaTest;
+
+public class CommonMultiple {
+    class Solution {
+        public int solution(int number, int n, int m) {
+            int answer = ((number%n==0)&&(number%m==0)?1:0);
+
+            return answer;
+        }
+    }
+}
