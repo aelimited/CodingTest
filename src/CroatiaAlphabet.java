c35ff5bab33399722bf657f639337e600f131de9
aelim <cloim09@gmail.com>
2024-01-08 23:27:09 +0900

    CroatiaAlphabet

--- src/CroatiaAlphabet.java
+++ src/CroatiaAlphabet.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class CroatiaAlphabet {
     public static void main(String[] args) {
+
         Scanner sc =new Scanner(System.in);
         String str = sc.nextLine();
         int cnt = 0;
